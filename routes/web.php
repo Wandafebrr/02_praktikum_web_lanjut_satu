0fb72005d61cec03123bbead6647d83b3c4806e1
Wandafebrr <wandafebrr239@gmail.com>
2023-02-26 14:56:33 +0700

    Jobsheet 2 praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AboutusController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactusController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\WandaController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,4 +40,24 @@ Route::get('/about',[AboutController::class,'about']);
 
 Route::get('/articles/{id}',[ArticleController::class,'articles']);
 
+//prak 3
+Route::prefix('product')->group(function(){
+    Route::get('/',[CategoryController::class,"index"]);
+    Route::get('/{id}',[CategoryController::class,"show"]);
+});
 
+Route::prefix('news')->group(function(){
+    Route::get('/',[NewsController::class,"index"]);
+    Route::get('/{id}',[NewsController::class,"show"]);
+});
+
+Route::prefix('program')->group(function(){
+    Route::get('/',[ProgramController::class,"index"]);
+    Route::get('/{id}',[ProgramController::class,"show"]);
+});
+
+Route::get('/about-us',[AboutusController::class,"index"]);
+
+Route::resource('/contact-us', ContactusController::class)->only([
+    'index', 'show'
+   ]);
